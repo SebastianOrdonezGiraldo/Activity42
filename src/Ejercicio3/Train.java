2bd617fab3acf436caf70bdef9656b8fcedcf801
Sebastian Ordoñez Giraldo <sebastian789go@gmail.com>
2024-02-18 21:26:54 -0500

    Ejercicio 5

--- src/Ejercicio3/Train.java
+++ src/Ejercicio3/Train.java
@@ -3,16 +3,23 @@ package Ejercicio3;
 import java.util.List;
 import java.util.ArrayList;
 
+
+
 public class Train {
     private String number;
     private String route;
-    private List<Wagon> wagonList = new ArrayList<>();
+    private List<String> wagonList = new ArrayList<>();
 
+    public Train(String number, String route, List<String> wagonList) {
+        this.number = number;
+        this.route = route;
+        this.wagonList = wagonList;
+    }
 
     public String obtenerNumero() { return this.number; }
     public String obtenerRuta() { return this.route; }
-    public Wagon obtenerVagon(String type) {
-        for (Wagon wagon : wagonList) {
+    public String obtenerVagon(String type) {
+        for (String wagon : wagonList) {
             if (wagon.obtenerTipo().equals(type)) {
                 return wagon;
             }
--- src/Ejercicio3/TreinMain.java
+++ src/Ejercicio3/TreinMain.java
@@ -1,4 +1,16 @@
 package Ejercicio3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TreinMain {
+    public static void main(String[] args){
+        List<String> vagones = new ArrayList<>();
+        vagones.add("1");
+        vagones.add("2");
+        vagones.add("3");
+        Train train = new Train("10","Alemania-España",vagones);
+
+        System.out.println(vagones);
+    }
 }
